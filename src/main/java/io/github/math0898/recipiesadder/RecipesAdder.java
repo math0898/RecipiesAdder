4b82892c950ae0e9bbde8244fbf47cf450d7a660
math0898 <25396616+math0898@users.noreply.github.com>
2022-02-14 11:03:19 -0600

    Started Recipes

---
+++ src/main/java/io/github/math0898/recipiesadder/RecipesAdder.java
@@ -0,0 +1,20 @@
+package io.github.math0898.recipiesadder;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * The main class of the RecipesAdder plugin. It's designed so that server owners can add and register as many item
+ * recipes as they desire. Does not support exact choice.
+ *
+ * @author Sugaku
+ */
+public final class RecipesAdder extends JavaPlugin {
+
+    /**
+     * Called on enable to load the recipes and then register them with spigot so that they can be used in game.
+     */
+    @Override
+    public void onEnable () {
+
+    }
+}
